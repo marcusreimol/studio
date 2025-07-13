96343897aad0318e3bcfd7d3a4f76be58787e910
Marcus Reimol <marcusreimol@gmail.com>
2025-07-13 21:36:36 +0000

    Nome do projeto Sindiclub ID do projeto sustentazona Número do projeto 1

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +19,7 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
-const signInWithGoogle = async () => {
+const signInWithGoogle = async (): Promise<User | null> => {
     try {
         const result = await signInWithPopup(auth, googleProvider);
         // The signed-in user info.
