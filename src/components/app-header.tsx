16be0c3ac3fce426d0d682ee0e6f3c4488187606
Marcus Reimol <marcusreimol@gmail.com>
2025-07-26 04:36:49 +0000

    Ainda não consegue buscar os dados, tire a barra de busca do header e co

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -13,7 +13,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { SidebarTrigger } from "./ui/sidebar";
-import { Search } from "lucide-react";
+import { Home, Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, signOut } from "@/lib/firebase";
@@ -33,16 +33,12 @@ export default function AppHeader() {
     <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6 sticky top-0 z-30">
         <SidebarTrigger className="md:hidden" />
         <div className="w-full flex-1">
-          <form>
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Buscar demandas..."
-                className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-              />
-            </div>
-          </form>
+            <Link href="/dashboard">
+              <Button variant="ghost" size="icon">
+                  <Home className="h-5 w-5" />
+                  <span className="sr-only">Dashboard</span>
+              </Button>
+            </Link>
         </div>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
