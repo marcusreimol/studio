a435137efdc51b44854db70f7f173fd3dbd7e399
Marcus Reimol <marcusreimol@gmail.com>
2025-07-28 01:46:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/demands/page.tsx
+++ src/app/(app)/demands/page.tsx
@@ -124,7 +124,7 @@ export default function DemandsPage() {
             <TableBody>
               {loading ? (
                 [...Array(5)].map((_, i) => (
-                  <TableRow key={`skeleton-${i}`}>
+                  <TableRow key={i}>
                     <TableCell><Skeleton className="h-5 w-48" /></TableCell>
                     <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                     <TableCell className="hidden md:table-cell"><Skeleton className="h-5 w-32" /></TableCell>
